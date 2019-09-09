6133140b9aaf50a9eeeee8936e00084c38ad0706
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-09-09 11:00:52 +0800

    test casks

---
+++ Casks/dotnet-sdk-2.2.300.rb
@@ -0,0 +1,30 @@
+cask 'dotnet-sdk-2.2.300' do
+  version '2.2.300,60aefe1e-e5af-4a40-8985-7e4d270a53aa:5033e8abbd2e218abf77207611be0504'
+  sha256 'f7ba19f3f30af6db1e9fb760ac9f9e7d1ce7acb1c204223a1c1bae02cbab493a'
+
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  appcast 'https://www.microsoft.com/net/download/macos'
+  name ".NET Core SDK #{version.before_comma}"
+  homepage 'https://www.microsoft.com/net/core#macos'
+
+  depends_on macos: '>= :sierra'
+
+  pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  #binary '/usr/local/share/dotnet/dotnet'
+
+  uninstall pkgutil: [
+                     "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64",
+                     "com.microsoft.dotnet.hostfxr.2.2.6.component.osx.x64",
+                     "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.2.2.6.component.osx.x64",
+                     "com.microsoft.dotnet.sharedhost.2.2.6.component.osx.x64"
+                   ],
+            # delete:  [
+            #            '/etc/paths.d/dotnet',
+            #            '/etc/paths.d/dotnet-cli-tools',
+            #          ]
+
+  zap trash: [
+               '~/.dotnet',
+               '~/.nuget',
+             ]
+end
---
+++ Casks/dotnet-sdk-2.2.401.rb
@@ -0,0 +1,36 @@
+cask 'dotnet-sdk-2.2.401' do
+  version '2.2.401,5e137e65-24c7-4f96-ac52-481e14eedcce:8a12628a2a3fd3fd96661f984bba658f'
+  sha256 'fe597fa739bbad9b1a1165f18a5c99b2e7157d6095be187445d3cfc829012965'
+
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  appcast 'https://www.microsoft.com/net/download/macos'
+  name ".NET Core SDK #{version.before_comma}"
+  homepage 'https://www.microsoft.com/net/core#macos'
+
+  conflicts_with cask: [
+                         'dotnet',
+                         'dotnet-preview',
+                         'dotnet-sdk-preview',
+                       ]
+
+  depends_on macos: '>= :sierra'
+
+  pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  #binary '/usr/local/share/dotnet/dotnet'
+
+  uninstall pkgutil: [
+                     "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64",
+                     "com.microsoft.dotnet.hostfxr.2.2.6.component.osx.x64",
+                     "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.2.2.6.component.osx.x64",
+                     "com.microsoft.dotnet.sharedhost.2.2.6.component.osx.x64"
+                   ],
+            # delete:  [
+            #            '/etc/paths.d/dotnet',
+            #            '/etc/paths.d/dotnet-cli-tools',
+            #          ]
+
+  zap trash: [
+               '~/.dotnet',
+               '~/.nuget',
+             ]
+end
