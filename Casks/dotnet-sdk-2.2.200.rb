ccdb04af06efb0640e7e74f63fcdcbbd65432f35
Isen <3904043+isen-ng@users.noreply.github.com>
2019-09-19 16:44:16 +0800

    Update auto-updater and setup nightly update and fix CI (#6)

---
+++ Casks/dotnet-sdk-2.2.200.rb
@@ -0,0 +1,24 @@
+cask 'dotnet-sdk-2.2.200' do
+  version '2.2.206,2.2.7'
+  sha256 '79648a5342a15927450b37c5ea3b79f3c5966bba369d438f50d0d52de8d44440'
+
+  url 'https://download.visualstudio.microsoft.com/download/pr/99c2d142-7782-4c78-9052-0d35d441af23/aadba3d0ee3ba5b407648d446580891b/dotnet-sdk-2.2.206-osx-x64.pkg'
+  name ".NET Core SDK #{version.before_comma}"
+  homepage 'https://www.microsoft.com/net/core#macos'
+
+  depends_on macos: '>= :sierra'
+
+  pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+
+  uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64"
+
+  zap trash:   [
+                 '~/.dotnet',
+                 '~/.nuget',
+               ],
+      pkgutil: [
+                 "com.microsoft.dotnet.hostfxr.#{version.after_comma}.component.osx.x64",
+                 "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.after_comma}.component.osx.x64",
+                 'com.microsoft.dotnet.sharedhost.component.osx.x64',
+               ]
+end
---
+++ travis-ci.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -u
+set -e
+
+any_casks_modified () {
+  [[ -n "$(modified_cask_files)" ]]
+}
+
+modified_cask_files () {
+  if [[ -z "${MODIFIED_CASK_FILES+defined}" ]]; then
+    MODIFIED_CASK_FILES="$(git diff --name-only --diff-filter=AM "${TRAVIS_COMMIT_RANGE}" -- Casks/*.rb)"
+    export MODIFIED_CASK_FILES
+  fi
+  echo "${MODIFIED_CASK_FILES}"
+}
+
+if any_casks_modified; then
+  modified_casks=($(modified_cask_files))
+
+  echo "Modified casks: ${modified_casks[@]}"
+  echo "------------------------------------"
+
+  if [ "${#modified_casks[@]}" -gt 1 ]; then
+    echo "More than one cask modified; please submit a pull request for each cask separately."
+    exit 0
+  fi
+
+  MODIFIED_CASK_FILE=${modified_casks[0]}
+
+  echo "Running brew audit $MODIFIED_CASK_FILE ..."
+  echo "------------------------------------"
+  brew cask audit $MODIFIED_CASK_FILE
+
+  echo "Running brew style $MODIFIED_CASK_FILE ..."
+  echo "------------------------------------"
+  brew cask style $MODIFIED_CASK_FILE
+
+  echo "Running brew install $MODIFIED_CASK_FILE ..."
+  echo "------------------------------------"
+  brew cask install $MODIFIED_CASK_FILE
+
+  echo "Running brew zap (uninstall will run implicity) $MODIFIED_CASK_FILE ..."
+  echo "------------------------------------"
+  brew cask zap $MODIFIED_CASK_FILE
+fi
