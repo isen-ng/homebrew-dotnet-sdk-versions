314752a0455e83fc8b40809ecc2fb2d2cf557dfd
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-09-19 17:19:43 +0800

    fix styles

--- Casks/dotnet-sdk-2.1.400.rb
+++ Casks/dotnet-sdk-2.1.400.rb
@@ -19,8 +19,8 @@ cask 'dotnet-sdk-2.1.400' do
       pkgutil: [
                  "com.microsoft.dotnet.hostfxr.#{version.after_comma}.component.osx.x64",
                  "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.after_comma}.component.osx.x64",
-                 'com.microsoft.dotnet.sharedhost.component.osx.x64'
-                ]
+                 'com.microsoft.dotnet.sharedhost.component.osx.x64',
+               ]
 
   caveats 'Uninstalling the offical dotnet-sdk casks will remove the shared runtime dependencies, '\
           'so you\'ll need to reinstall the particular version cask you want from this tap again '\
