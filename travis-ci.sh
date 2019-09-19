91aeabdaec9bb0b71482a2f55eec9696e3da0522
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-09-19 15:24:49 +0800

    travis ci

---
+++ travis-ci.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -u
+set -e
+
+any_casks_modified () {
+  [[ -n "$(modified_cask_files)" ]]
+}
+
+modified_cask_files () {
+  if [[ -z "${MODIFIED_CASK_FILES+defined}" ]]; then
+    MODIFIED_CASK_FILES="$(git diff --name-only --diff-filter=AM "${TRAVIS_COMMIT_RANGE}" -- Casks/*.rb)"
+    export MODIFIED_CASK_FILES
+  fi
+  echo "${MODIFIED_CASK_FILES}"
+}
+
+if any_casks_modified; then
+  modified_casks=($(modified_cask_files))
+
+  echo "Modified casks: ${modified_casks[@]}"
+
+  if [ "${#modified_casks[@]}" -gt 1 ]; then
+    echo "More than one cask modified; please submit a pull request for each cask separately."
+    exit 0
+  fi
+
+  MODIFIED_CASK_FILE=${modified_casks[0]}
+
+  echo "Running brew audit $MODIFIED_CASK_FILE ..."
+  brew audit $MODIFIED_CASK_FILE
+
+  echo "Running brew style $MODIFIED_CASK_FILE ..."
+  brew style $MODIFIED_CASK_FILE
+
+  echo "Running brew install $MODIFIED_CASK_FILE ..."
+  brew install $MODIFIED_CASK_FILE
+
+  echo "Running brew uninstall $MODIFIED_CASK_FILE ..."
+  brew uninstall $MODIFIED_CASK_FILE
+fi
