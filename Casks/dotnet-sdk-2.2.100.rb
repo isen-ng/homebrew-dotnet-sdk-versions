420bc6c372a8181497e055744cbcfba51dfd5935
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-09-19 20:51:12 +0800

    Update 2.2.100

--- Casks/dotnet-sdk-2.2.100.rb
+++ Casks/dotnet-sdk-2.2.100.rb
@@ -1,26 +1,28 @@
-cask "dotnet-sdk-2.2.100" do
-  version "2.2.107,2.2.5"
-  sha256 "a163baac1a6e447367b67726669a3cf42b1a65c2f374e92c28c38d6753c210d2"
+cask 'dotnet-sdk-2.2.100' do
+  version '2.2.107,2.2.5'
+  sha256 'a163baac1a6e447367b67726669a3cf42b1a65c2f374e92c28c38d6753c210d2'
 
-  url "https://download.visualstudio.microsoft.com/download/pr/d3392a3f-8437-4357-8ed1-58061a421ab7/4e354d4a659d1ac4b20ebacb2dedf656/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
-  appcast "https://www.microsoft.com/net/download/macos"
+  url 'https://download.visualstudio.microsoft.com/download/pr/d3392a3f-8437-4357-8ed1-58061a421ab7/4e354d4a659d1ac4b20ebacb2dedf656/dotnet-sdk-2.2.107-osx-x64.pkg'
   name ".NET Core SDK #{version.before_comma}"
-  homepage "https://www.microsoft.com/net/core#macos"
+  homepage 'https://www.microsoft.com/net/core#macos'
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: '>= :sierra'
 
   pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
 
   uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64"
 
-  zap trash: [
-    "~/.dotnet",
-    "~/.nuget"
-  ],
+  zap trash:   [
+                 '~/.dotnet',
+                 '~/.nuget',
+               ],
       pkgutil: [
-        "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64",
-        "com.microsoft.dotnet.hostfxr.#{version.after_comma.before_colon}.component.osx.x64",
-        "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.after_comma.before_colon}.component.osx.x64",
-        "com.microsoft.dotnet.sharedhost.component.osx.x64"
-      ]
+                 "com.microsoft.dotnet.hostfxr.#{version.after_comma}.component.osx.x64",
+                 "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.after_comma}.component.osx.x64",
+                 'com.microsoft.dotnet.sharedhost.component.osx.x64',
+               ]
+
+  caveats 'Uninstalling the offical dotnet-sdk casks will remove the shared runtime dependencies, '\
+          'so you\'ll need to reinstall the particular version cask you want from this tap again '\
+          'for the `dotnet` command to work again.'
 end
