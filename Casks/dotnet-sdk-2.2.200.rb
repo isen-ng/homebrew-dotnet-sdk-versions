5d130df7f743a940aeb09de89cf036c4a04e41fc
Isen Ng <3904043+isengrim613@users.noreply.github.com>
2019-09-19 11:47:37 +0800

    more style

--- Casks/dotnet-sdk-2.2.200.rb
+++ Casks/dotnet-sdk-2.2.200.rb
@@ -12,17 +12,15 @@ cask 'dotnet-sdk-2.2.200' do
 
   uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64"
 
-  zap 
-  trash:
-  [
-    '~/.dotnet',
-    '~/.nuget',
-  ],
-  pkgutil:
-  [
-    "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64",
-    "com.microsoft.dotnet.hostfxr.#{version.after_comma.before_colon}.component.osx.x64",
-    "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.after_comma.before_colon}.component.osx.x64",
-    'com.microsoft.dotnet.sharedhost.component.osx.x64',
-  ]
+  zap trash:  [
+                '~/.dotnet',
+                '~/.nuget',
+              ],
+      pkgutil:
+              [
+                "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64",
+                "com.microsoft.dotnet.hostfxr.#{version.after_comma}.component.osx.x64",
+                "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.after_comma}.component.osx.x64",
+                'com.microsoft.dotnet.sharedhost.component.osx.x64',
+              ]
 end
