1c8833261cc6c515effa6ff84664b3de05c0daf6
Isen <3904043+isen-ng@users.noreply.github.com>
2020-01-21 16:49:55 +0800

    update dotnet-sdk-3.0.100.rb from 3.0.101,3.0.1 to 3.0.102,3.0.2 (#45)

--- Casks/dotnet-sdk-3.0.100.rb
+++ Casks/dotnet-sdk-3.0.100.rb
@@ -1,8 +1,8 @@
 cask 'dotnet-sdk-3.0.100' do
-  version '3.0.101,3.0.1'
-  sha256 '9d40f99047c2713efcf48c67c80b3bac0fcd24c87bdf7263b8e37e3bdf9901e9'
+  version '3.0.102,3.0.2'
+  sha256 'd925f90066492021b64254962c38d48208a4d85a687aee95b2617f6329ca3ab9'
 
-  url 'https://download.visualstudio.microsoft.com/download/pr/1b9f265d-ba27-4f0c-8b4d-1fd42bd8448b/2bbd64abddeeea91149df3aa39d049ae/dotnet-sdk-3.0.101-osx-x64.pkg'
+  url 'https://download.visualstudio.microsoft.com/download/pr/3234b6b7-a3da-4e02-ad2a-63d75975f1dc/5a0a2d20b24cadf3e68ba179d4598fa6/dotnet-sdk-3.0.102-osx-x64.pkg'
   name ".NET Core SDK #{version.before_comma}"
   homepage 'https://www.microsoft.com/net/core#macos'
 
