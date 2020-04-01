c9e0991d9570b439bfb7d604609df21fb064b691
Isen <3904043+isen-ng@users.noreply.github.com>
2020-04-01 21:13:52 +0800

    update dotnet-sdk-2.1.800.rb from 2.1.804,2.1.16 to 2.1.805,2.1.17 (#52)

--- Casks/dotnet-sdk-2.1.800.rb
+++ Casks/dotnet-sdk-2.1.800.rb
@@ -1,8 +1,8 @@
 cask 'dotnet-sdk-2.1.800' do
-  version '2.1.804,2.1.16'
-  sha256 '2c07eec50fc0fd35cffe37533f749d2dbe9986327d56b2da32cbbb1621ad1a2e'
+  version '2.1.805,2.1.17'
+  sha256 '3f9d2960f92665b68b79967b1092f638dcdca05f79a2e3cde659fa0566c931ed'
 
-  url 'https://download.visualstudio.microsoft.com/download/pr/f845905d-89d4-4e47-b2ec-398e47ff8411/2f49da28650e5259e694aa7321dcf935/dotnet-sdk-2.1.804-osx-x64.pkg'
+  url 'https://download.visualstudio.microsoft.com/download/pr/1f42da0e-a749-4338-ac39-5160a1f31ab0/74f8b6c934b0dc3024f336460c89e8ad/dotnet-sdk-2.1.805-osx-x64.pkg'
   name ".NET Core SDK #{version.before_comma}"
   homepage 'https://www.microsoft.com/net/core#macos'
 
