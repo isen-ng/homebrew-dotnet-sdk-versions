7debc134e8301a641e41d5007d3c183296861703
Isen <3904043+isen-ng@users.noreply.github.com>
2020-08-04 10:38:17 +0800

    new brew style for 2.2.100 (#71)

--- Casks/dotnet-sdk-2.2.100.rb
+++ Casks/dotnet-sdk-2.2.100.rb
@@ -1,28 +1,25 @@
-cask 'dotnet-sdk-2.2.100' do
-  version '2.2.110,2.2.8'
-  sha256 'e093003cfe7cd6d7bdc517b9a17dfff95dab96dee79c6b843849fac511087d97'
+cask "dotnet-sdk-2.2.100" do
+  version "2.2.110,2.2.8"
+  sha256 "e093003cfe7cd6d7bdc517b9a17dfff95dab96dee79c6b843849fac511087d97"
 
-  url 'https://download.visualstudio.microsoft.com/download/pr/7db9e4c2-7118-4c13-8689-4193e4c91aed/a783f5cad3c017097bc123b478eee2a3/dotnet-sdk-2.2.110-osx-x64.pkg'
+  url "https://download.visualstudio.microsoft.com/download/pr/7db9e4c2-7118-4c13-8689-4193e4c91aed/a783f5cad3c017097bc123b478eee2a3/dotnet-sdk-2.2.110-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
-  homepage 'https://www.microsoft.com/net/core#macos'
+  homepage "https://www.microsoft.com/net/core#macos"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
   pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
 
   uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64"
 
-  zap trash:   [
-                 '~/.dotnet',
-                 '~/.nuget',
-               ],
+  zap trash:   ["~/.dotnet", "~/.nuget"],
       pkgutil: [
-                 "com.microsoft.dotnet.hostfxr.#{version.after_comma}.component.osx.x64",
-                 "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.after_comma}.component.osx.x64",
-                 'com.microsoft.dotnet.sharedhost.component.osx.x64',
-               ]
+        "com.microsoft.dotnet.hostfxr.#{version.after_comma}.component.osx.x64",
+        "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.after_comma}.component.osx.x64",
+        "com.microsoft.dotnet.sharedhost.component.osx.x64",
+      ]
 
-  caveats 'Uninstalling the offical dotnet-sdk casks will remove the shared runtime dependencies, '\
-          'so you\'ll need to reinstall the particular version cask you want from this tap again '\
-          'for the `dotnet` command to work again.'
+  caveats "Uninstalling the offical dotnet-sdk casks will remove the shared runtime dependencies, "\
+          "so you\'ll need to reinstall the particular version cask you want from this tap again "\
+          "for the `dotnet` command to work again."
 end
