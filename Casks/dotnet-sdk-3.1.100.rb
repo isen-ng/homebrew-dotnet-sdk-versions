9fc2e14d812c2912a4a82e8c2614145bbe06ff50
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2020-09-17 11:01:06 +0800

    adhere to brew doctor

--- Casks/dotnet-sdk-3.1.100.rb
+++ Casks/dotnet-sdk-3.1.100.rb
@@ -1,9 +1,10 @@
-cask "dotnet-sdk-3.1.100" do
+cask "dotnet-sdk3-1-100" do
   version "3.1.108,3.1.8"
   sha256 "6e50afa958c61344069d2a3aaf0e9e7e04d8fdae71718071afa5221f1bdc97e0"
 
   url "https://download.visualstudio.microsoft.com/download/pr/dfa54ec7-bf38-401c-b52d-9b5484374378/d437385f9f5c1beb1b5f79f04c389b64/dotnet-sdk-3.1.108-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
+  desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
 
   depends_on macos: "> :sierra"
