95e41f2d21d4589d1f95ad4c542a30c7533dd3cb
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2020-09-17 10:54:56 +0800

    adhere to brew audit

--- Casks/dotnet-sdk2-1-500.rb
+++ Casks/dotnet-sdk2-1-500.rb
@@ -1,9 +1,10 @@
-cask "dotnet-sdk-2.1.500" do
+cask "dotnet-sdk2-1-500" do
   version "2.1.518,2.1.22"
   sha256 "dd570e5793627f8e17edccfcbf3d75391d4a9704b1060da4ba94d9065b557eed"
 
   url "https://download.visualstudio.microsoft.com/download/pr/95486566-6122-44a9-bc1f-c6144f69fa2f/b67c569f875bc0b1aa0b386f36533de5/dotnet-sdk-2.1.518-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
+  desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
 
   depends_on macos: ">= :sierra"
