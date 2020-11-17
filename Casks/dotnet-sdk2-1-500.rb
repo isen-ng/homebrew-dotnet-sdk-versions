77e3cec4a614c081e6eef87d7e06b0bb359ee8f9
Isen <3904043+isen-ng@users.noreply.github.com>
2020-11-17 15:04:43 +0800

    [Auto] update dotnet-sdk2-1-500.rb from 2.1.518,2.1.22 to 2.1.519,2.1.23 (#107)

--- Casks/dotnet-sdk2-1-500.rb
+++ Casks/dotnet-sdk2-1-500.rb
@@ -1,8 +1,8 @@
 cask "dotnet-sdk2-1-500" do
-  version "2.1.518,2.1.22"
-  sha256 "dd570e5793627f8e17edccfcbf3d75391d4a9704b1060da4ba94d9065b557eed"
+  version "2.1.519,2.1.23"
+  sha256 "99a5b8d4bd3856706a091f06db676808890c5aaabf84cdf327e62483aa0f40e4"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/95486566-6122-44a9-bc1f-c6144f69fa2f/b67c569f875bc0b1aa0b386f36533de5/dotnet-sdk-2.1.518-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/7871387c-aea9-4228-b703-f81bfe3b41ca/217ff603d32ec97fa604e9b2a52e2e95/dotnet-sdk-2.1.519-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
