c60b645dbdf049778a26d95548e3fdd0c118b739
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2021-01-18 19:08:14 +0000

    [Auto] update dotnet-sdk2-1-500.rb from 2.1.519,2.1.23 to 2.1.520,2.1.24

--- Casks/dotnet-sdk2-1-500.rb
+++ Casks/dotnet-sdk2-1-500.rb
@@ -1,8 +1,8 @@
 cask "dotnet-sdk2-1-500" do
-  version "2.1.519,2.1.23"
-  sha256 "99a5b8d4bd3856706a091f06db676808890c5aaabf84cdf327e62483aa0f40e4"
+  version "2.1.520,2.1.24"
+  sha256 "ce2a373b10990da8a76cd634ade497d018e3f7be8230469d506a7d22f1dd9c1e"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/7871387c-aea9-4228-b703-f81bfe3b41ca/217ff603d32ec97fa604e9b2a52e2e95/dotnet-sdk-2.1.519-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/ef83ee74-16f9-4e39-97c9-07fdbe243adf/b9fac726eec971defc782400ccfd5e33/dotnet-sdk-2.1.520-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
