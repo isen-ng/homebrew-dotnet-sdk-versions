e3e9368bd4bb4508eea17725d16d8346ce21aefd
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2021-01-21 10:03:14 +0800

    forcefully add interpolation in url to workaround brew audit

--- Casks/dotnet-sdk3-1-100.rb
+++ Casks/dotnet-sdk3-1-100.rb
@@ -2,7 +2,7 @@ cask "dotnet-sdk3-1-100" do
   version "3.1.111,3.1.11"
   sha256 "366717614dc5393b01d091a40a5a8838b5d00efe07cb76171b8dcf722fa5bdb1"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/a94833ea-b78e-4985-8f32-8bd36b51a598/6dd5e3719633f8bda5ea6894353dbe51/dotnet-sdk-3.1.111-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/a94833ea-b78e-4985-8f32-8bd36b51a598/6dd5e3719633f8bda5ea6894353dbe51/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
