61caa15d56bd84595e66a1c67ba72e46c1d92225
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2021-02-24 21:33:25 +0800

    fix brew audit

--- Casks/dotnet-sdk2-2-100.rb
+++ Casks/dotnet-sdk2-2-100.rb
@@ -2,7 +2,7 @@ cask "dotnet-sdk2-2-100" do
   version "2.2.110,2.2.8"
   sha256 "e093003cfe7cd6d7bdc517b9a17dfff95dab96dee79c6b843849fac511087d97"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/7db9e4c2-7118-4c13-8689-4193e4c91aed/a783f5cad3c017097bc123b478eee2a3/dotnet-sdk-2.2.110-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/7db9e4c2-7118-4c13-8689-4193e4c91aed/a783f5cad3c017097bc123b478eee2a3/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
