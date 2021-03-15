8b7958d14cc7568ee2fee67e878f2907ca3d9472
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2021-03-15 19:08:18 +0000

    [Auto] update dotnet-sdk2-1-800.rb from 2.1.813,2.1.25 to 2.1.814,2.1.26

--- Casks/dotnet-sdk2-1-800.rb
+++ Casks/dotnet-sdk2-1-800.rb
@@ -1,6 +1,6 @@
 cask "dotnet-sdk2-1-800" do
-  version "2.1.813,2.1.25"
-  sha256 "906386b661b081b394988458691c90f4dc6e2f0ea6121a8877df66260566e4a2"
+  version "2.1.814,2.1.26"
+  sha256 "7ba2c99a99ec89e65e6593c881eb126a15ed385386efc4d651b85e9b8110ec61"
 
   url "https://download.visualstudio.microsoft.com/download/pr/1c7f880e-9cfb-46f9-9e6c-1ead2d713d37/54efd298bd08ff1c52d6c9362335da48/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
