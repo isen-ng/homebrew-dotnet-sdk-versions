1b496c3130e28a6510f5302e86dfd8888cde2c8b
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2021-03-16 23:04:57 +0800

    fix url that wasn't updated

--- Casks/dotnet-sdk2-1-500.rb
+++ Casks/dotnet-sdk2-1-500.rb
@@ -2,7 +2,7 @@ cask "dotnet-sdk2-1-500" do
   version "2.1.522,2.1.26"
   sha256 "96e352824bff3f3e6d28bdcdf91133ce0982408a20162dd8361611b8bbf20ac5"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/b94055bb-450c-4a20-ad7c-2df326311cd1/28c6da582f6bc9f7ce157811703e46fb/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/bfaecd11-bc61-4c69-8f73-3aad45a5e377/7d21d24fd4ebc6c3015e5622a1d5ec8d/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
