bb03f1d4ca6529f28404b9cdef3f76695d540554
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2021-03-16 23:09:29 +0800

    fix cask url

--- Casks/dotnet-sdk3-1-400.rb
+++ Casks/dotnet-sdk3-1-400.rb
@@ -2,7 +2,7 @@ cask "dotnet-sdk3-1-400" do
   version "3.1.407,3.1.13"
   sha256 "e65bd0a99365d494a07983f33346597db220dc3e199acc1aedde3dd0d3795750"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/0b119bb9-4be3-4ea1-9702-529a70339af0/84aeed5be73b8873d7e7eafb323e955e/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/ecd4efe6-5f37-4183-82a1-dd2de88ca573/144bb23aab65c4439d0600973d756e6b/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
