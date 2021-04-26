ae3473fe88257af89ace848b75486be6f2d81fa3
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2021-04-26 14:00:16 +0800

    disable livecheck

--- Casks/dotnet-sdk2-2-300.rb
+++ Casks/dotnet-sdk2-2-300.rb
@@ -7,6 +7,10 @@ cask "dotnet-sdk2-2-300" do
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
 
+  livecheck do
+    skip "See https://github.com/isen-ng/homebrew-dotnet-sdk-versions/blob/master/CONTRIBUTING.md#automatic-updates"
+  end
+
   depends_on macos: ">= :sierra"
 
   pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
