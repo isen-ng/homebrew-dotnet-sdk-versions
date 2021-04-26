759ccacc8e033e551b5758875b02f1270059e389
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2021-04-26 12:03:49 +0800

    disable livecheck

--- Casks/dotnet-sdk2-1-500.rb
+++ Casks/dotnet-sdk2-1-500.rb
@@ -3,10 +3,13 @@ cask "dotnet-sdk2-1-500" do
   sha256 "288519b6fc87f245c4e53212b4629f7e887b7d49617a1899b202760e7caf092b"
 
   url "https://download.visualstudio.microsoft.com/download/pr/86a6d223-85fb-4d80-98bb-e45cab9926db/66bbeeb674f783e70b2b5b672d2a4972/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
-  appcast "https://github.com/dotnet/sdk/releases.atom"
   name ".NET Core SDK #{version.before_comma}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
+
+  livecheck do
+    skip "See https://github.com/isen-ng/homebrew-dotnet-sdk-versions"
+  end
 
   depends_on macos: ">= :sierra"
 
