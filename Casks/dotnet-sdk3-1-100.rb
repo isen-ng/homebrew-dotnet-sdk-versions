6ac61cbe6ebc6dcc9871e5b5059c07956c8cd49c
Isen <3904043+isen-ng@users.noreply.github.com>
2021-07-21 03:06:50 +0800

    [Auto] update dotnet-sdk3-1-100.rb from 3.1.116,3.1.16 to 3.1.117,3.1.17 (#153)

--- Casks/dotnet-sdk3-1-100.rb
+++ Casks/dotnet-sdk3-1-100.rb
@@ -1,8 +1,8 @@
 cask "dotnet-sdk3-1-100" do
-  version "3.1.116,3.1.16"
-  sha256 "948e744d8e6d157e5c9cc74b6837d179ac0b23e3ce9b90474abb1d45dc4cd6c3"
+  version "3.1.117,3.1.17"
+  sha256 "7ff4bbdaf6f8a88c5929e2af3c24f3add7d62fc730b1ece2d43efcb84e59a968"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/53fd499d-69d8-4a0e-9587-6ac59dfaff73/b3d993a35a2edf36eaf8410f0719ac03/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/298924b2-2598-4cc0-b0a3-e30189b82e31/e2cf7067074dab9043c7a2d4ad9fdd01/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   name ".NET Core SDK #{version.before_comma}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
