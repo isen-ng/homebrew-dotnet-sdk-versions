af08b38d9ea725df0cf82afbdbf8f019eb4eb377
Bas Stottelaar <basilfx@users.noreply.github.com>
2021-11-09 08:58:17 +0100

    Add dotnet-sdk6-0-100 (#175)

---
+++ Casks/dotnet-sdk6-0-100.rb
@@ -0,0 +1,30 @@
+cask "dotnet-sdk6-0-100" do
+  version "6.0.100,6.0.0"
+  sha256 "9203560506408d8f88774358b03cdcfcfa0495682fde6034b24f7ccaeddce2ef"
+
+  url "https://download.visualstudio.microsoft.com/download/pr/14a45451-4cc9-48e1-af69-0aff75891d09/ff6e83986a2a9a535015fb3104a90a1b/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  name ".NET Core SDK #{version.before_comma}"
+  desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
+  homepage "https://www.microsoft.com/net/core#macos"
+
+  livecheck do
+    skip "See https://github.com/isen-ng/homebrew-dotnet-sdk-versions"
+  end
+
+  depends_on macos: "> :sierra"
+
+  pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+
+  uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64"
+
+  zap trash:   ["~/.dotnet", "~/.nuget"],
+      pkgutil: [
+        "com.microsoft.dotnet.hostfxr.#{version.after_comma}.component.osx.x64",
+        "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.after_comma}.component.osx.x64",
+        "com.microsoft.dotnet.sharedhost.component.osx.x64",
+      ]
+
+  caveats "Uninstalling the offical dotnet-sdk casks will remove the shared runtime dependencies, "\
+          "so you\'ll need to reinstall the particular version cask you want from this tap again "\
+          "for the `dotnet` command to work again."
+end
