965565b7d52725615c46b8b174eb4a31de871c52
Isen <3904043+isen-ng@users.noreply.github.com>
2022-02-15 10:08:36 +0800

    Fix breaking change in homebrew style again (#200)

--- Casks/dotnet-sdk2-1-500.rb
+++ Casks/dotnet-sdk2-1-500.rb
@@ -2,8 +2,8 @@ cask "dotnet-sdk2-1-500" do
   version "2.1.526,2.1.30"
   sha256 "1cc10c220534fd27d8e76f1199ac18248b702916065d659073c7a6a7e1c26bc8"
 
-  url "https://download.visualstudio.microsoft.com/download/pr/97913c52-a78c-4c93-abe4-a2cdf3c933ee/dc2262512c0ac6bd80070b39e28594e6/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
-  name ".NET Core SDK #{version.before_comma}"
+  url "https://download.visualstudio.microsoft.com/download/pr/97913c52-a78c-4c93-abe4-a2cdf3c933ee/dc2262512c0ac6bd80070b39e28594e6/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
+  name ".NET Core SDK #{version.csv.first}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://www.microsoft.com/net/core#macos"
 
@@ -13,14 +13,14 @@ cask "dotnet-sdk2-1-500" do
 
   depends_on macos: ">= :sierra"
 
-  pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  pkg "dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
 
-  uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.before_comma}.component.osx.x64"
+  uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.csv.first}.component.osx.x64"
 
   zap trash:   ["~/.dotnet", "~/.nuget", "/etc/paths.d/dotnet", "/etc/paths.d/dotnet-cli-tools"],
       pkgutil: [
-        "com.microsoft.dotnet.hostfxr.#{version.after_comma}.component.osx.x64",
-        "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.after_comma}.component.osx.x64",
+        "com.microsoft.dotnet.hostfxr.#{version.csv.second}.component.osx.x64",
+        "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.csv.second}.component.osx.x64",
         "com.microsoft.dotnet.sharedhost.component.osx.x64",
       ]
 
