fc68783f82b68b9d8fef74bb12cb6bace158a515
isen <isen.ng@adyen.com>
2022-05-18 12:10:54 +0800

    Update 6 to mojave

--- Casks/dotnet-sdk6-0-200.rb
+++ Casks/dotnet-sdk6-0-200.rb
@@ -23,7 +23,7 @@ cask "dotnet-sdk6-0-200" do
     skip "See https://github.com/isen-ng/homebrew-dotnet-sdk-versions/blob/master/CONTRIBUTING.md#automatic-updates"
   end
 
-  depends_on macos: "> :sierra"
+  depends_on macos: "> :mojave"
 
   pkg "dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
 
