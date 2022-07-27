d86061bbdd259b8d9be6b3bcde49971e9d3bdaa8
danslapman <danslapman@gmail.com>
2022-07-27 09:24:28 +0300

    Dotnet SDK 6.0.3xx (#217)

---
+++ Casks/dotnet-sdk6-0-300.rb
@@ -0,0 +1,43 @@
+cask "dotnet-sdk6-0-300" do
+  version "6.0.302,6.0.7"
+
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+  sha256_x64 = "863a6b5a8ba6b0b2c1674bc952358e31c33cb0f4d98e8e6767241642bd1db9d1"
+  sha256_arm64 = "3f8acce92fd2fb1bafc9742fde3e389e7f3d609435052e7209f3ba4cdaee4228"
+  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/7257dada-8ec1-4b5c-9b69-7201a2cf377f/89f452882fb87fbb89d697417cb3f231/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
+  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/7d98e8ca-e8ec-490b-8ffc-55a458981d86/32c92f3aa0f460119de53477cffa8a0a/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
+
+  if Hardware::CPU.intel?
+    sha256 sha256_x64
+    url url_x64
+  else
+    sha256 sha256_arm64
+    url url_arm64
+  end
+
+  name ".NET Core SDK #{version.csv.first}"
+  desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
+  homepage "https://www.microsoft.com/net/core#macos"
+
+  livecheck do
+    skip "See https://github.com/isen-ng/homebrew-dotnet-sdk-versions/blob/master/CONTRIBUTING.md#automatic-updates"
+  end
+
+  depends_on macos: ">= :mojave"
+
+  pkg "dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
+
+  uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.csv.first}.component.osx.#{arch}"
+
+  zap trash:   ["~/.dotnet", "~/.nuget", "/etc/paths.d/dotnet", "/etc/paths.d/dotnet-cli-tools"],
+      pkgutil: [
+        "com.microsoft.dotnet.hostfxr.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.pack.apphost.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.sharedhost.component.osx.#{arch}",
+      ]
+
+  caveats "Uninstalling the offical dotnet-sdk casks will remove the shared runtime dependencies, " \
+          "so you\'ll need to reinstall the particular version cask you want from this tap again " \
+          "for the `dotnet` command to work again."
+end
--- README.md
+++ README.md
@@ -22,6 +22,7 @@ dotnet --list-sdks
 
 | Version             | DotNet SDK     | Arch        | Remarks
 |---------------------|----------------|-------------|---------
+| `dotnet-sdk6-0-300` | dotnet 6.0.302 | x64 & arm64 |
 | `dotnet-sdk6-0-200` | dotnet 6.0.202 | x64 & arm64 |
 | `dotnet-sdk6-0-100` | dotnet 6.0.102 | x64 & arm64 |
 | `dotnet-sdk5-0-400` | dotnet 5.0.408 | x64         | When installed on arm64, use `dotnetx64`
