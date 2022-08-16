764b8afcfc7b64997dbe40194eba37440668c96f
isen <isen.ng@adyen.com>
2022-08-16 14:15:56 +0800

    Use new arch and on_arch keywords

--- Casks/dotnet-sdk6-0-100.rb
+++ Casks/dotnet-sdk6-0-100.rb
@@ -1,16 +1,17 @@
 cask "dotnet-sdk6-0-100" do
   version "6.0.102,6.0.2"
 
-  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+  arch arm: "arm64", intel: "x64"
   sha256_x64 = "09e5bf1946b3892cc9f3bcba495e0524b849e006900332e0f58baeca8498121a"
   sha256_arm64 = "9222af1505b3698c2cc929110b4b068ad99ede3291c4e365dc8c59c6b0c0d5c9"
   url_x64 = "https://download.visualstudio.microsoft.com/download/pr/8509554d-61b4-43b8-b934-ad2e679ce18f/aa565a52b909b3133ef6763bb2868a49/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
   url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/cc2a94b1-7f3c-44f8-a842-f288a0cff04e/32446a93655522a1f933d4afb5e15836/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 sha256_x64
     url url_x64
-  else
+  end
+  on_arm do
     sha256 sha256_arm64
     url url_arm64
   end
