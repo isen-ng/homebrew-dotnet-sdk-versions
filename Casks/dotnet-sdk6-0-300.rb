ffeeb91d6345be8af9a2699e1aa5d9a409a5a559
Daniel Slapman <danslapman@gmail.com>
2022-08-26 16:35:02 +0300

    Update 6.0.x series

--- Casks/dotnet-sdk6-0-300.rb
+++ Casks/dotnet-sdk6-0-300.rb
@@ -1,12 +1,12 @@
 cask "dotnet-sdk6-0-300" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.0.302,6.0.7"
+  version "6.0.303,6.0.8"
 
-  sha256_x64 = "863a6b5a8ba6b0b2c1674bc952358e31c33cb0f4d98e8e6767241642bd1db9d1"
-  sha256_arm64 = "3f8acce92fd2fb1bafc9742fde3e389e7f3d609435052e7209f3ba4cdaee4228"
-  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/7257dada-8ec1-4b5c-9b69-7201a2cf377f/89f452882fb87fbb89d697417cb3f231/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
-  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/7d98e8ca-e8ec-490b-8ffc-55a458981d86/32c92f3aa0f460119de53477cffa8a0a/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
+  sha256_x64 = "3b0003931501345e76109c154dd0d0b0f6143eaef694835cfc038fc5ad09f667"
+  sha256_arm64 = "a3c434e8c6adff9867c3f37677edce221096f3d818489ea712bcec741459decb"
+  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/c0d44160-dce6-40e3-b55b-1dd4d96dc8be/9e3f268f52015ff49358820bee445c92/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
+  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/9a3d1b63-6fe8-49ca-91a9-77cb157e61b6/c5ea1853ea40dcfe228cf4ebe32db0d2/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
 
   on_intel do
     sha256 sha256_x64
