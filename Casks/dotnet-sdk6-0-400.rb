7d0c34e58fc2dcd866bdc5bd5b706c694c6f09c3
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2023-09-12 17:15:44 +0000

    [Auto] update Casks/dotnet-sdk6-0-400.rb from 6.0.413 to 6.0.414

--- Casks/dotnet-sdk6-0-400.rb
+++ Casks/dotnet-sdk6-0-400.rb
@@ -1,12 +1,12 @@
 cask "dotnet-sdk6-0-400" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.0.413,6.0.21"
+  version "6.0.414,6.0.22"
 
-  sha256_x64 = "923595db0d47b07cc2122cb850b68ea52fb9de333c61c66a0a87b45aaa633809"
-  sha256_arm64 = "f189791a8ece710469d51d8f4195437de2b83a8841881c001fd4ce039f922f96"
-  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/f8ef7ef6-04f0-4d7f-a39d-127d7cf099a9/021922f4a6860bcd8257df91a53cb5d0/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
-  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/d95d315f-8088-440b-88db-1e84398f6167/0a450912f02c207430bcc4bc9aaeefc1/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
+  sha256_x64 = "0bb807630047ec7afc4014b810bf4d859e0d7a5637996959a5efb4f72efee95d"
+  sha256_arm64 = "ac440601e3bef9bf824958df5288e44e5e9e0e32f3b6fd7ecb2022f29a7a5dcb"
+  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/1de3975a-e76b-4524-9584-b01d2c8db33e/4a05072624690d3b0167493cbb3d7757/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
+  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/6936c1ed-530d-4b1d-898e-1f27e5109c5a/a8f73f24b9103d0a64ca42f1462b0954/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
 
   on_arm do
     sha256 sha256_arm64
--- README.md
+++ README.md
@@ -28,7 +28,7 @@ dotnet --list-sdks
 | `dotnet-sdk7-0-300` | 7.0.306  | x64 & arm64 |                                          |
 | `dotnet-sdk7-0-200` | 7.0.203  | x64 & arm64 |                                          |
 | `dotnet-sdk7-0-100` | 7.0.102  | x64 & arm64 |                                          |
-| `dotnet-sdk6-0-400` | 6.0.413  | x64 & arm64 |                                          |
+| `dotnet-sdk6-0-400` | 6.0.414  | x64 & arm64 |                                          |
 | `dotnet-sdk6-0-300` | 6.0.303  | x64 & arm64 |                                          |
 | `dotnet-sdk6-0-200` | 6.0.202  | x64 & arm64 |                                          |
 | `dotnet-sdk6-0-100` | 6.0.108  | x64 & arm64 |                                          |
