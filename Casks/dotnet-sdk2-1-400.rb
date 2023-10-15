bcae30969e6adc85b74f02baf3c78069fe9aa821
Isen <3904043+isen-ng@users.noreply.github.com>
2023-10-15 15:11:46 +0800

    Update dotnet-sdk2-1-400.rb

--- Casks/dotnet-sdk2-1-400.rb
+++ Casks/dotnet-sdk2-1-400.rb
@@ -1,4 +1,6 @@
 cask "dotnet-sdk2-1-400" do
+  depends_on macos: ">= :sierra"
+
   version "2.1.403,2.1.5"
   sha256 "d290cefddb4fbdf1215724c049d86b4ce09f5dc2c5a658e3c1645c368f34c31a"
 
@@ -11,11 +13,9 @@ cask "dotnet-sdk2-1-400" do
     skip "See https://github.com/isen-ng/homebrew-dotnet-sdk-versions/blob/master/CONTRIBUTING.md#automatic-updates"
   end
 
-  depends_on macos: ">= :sierra"
-
   pkg "dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
 
-  uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.csv.first}.component.osx.x64"
+  uninstall??? pkgutil: "com.microsoft.dotnet.dev.#{version.csv.first}.component.osx.x64"
 
   zap trash:   ["~/.dotnet", "~/.nuget", "/etc/paths.d/dotnet", "/etc/paths.d/dotnet-cli-tools"],
       pkgutil: [
