f8e0fd7c81cedc7ed0a36b1e21895c69b2c7c347
Benjamin Evenson <2031163+benjiro@users.noreply.github.com>
2024-02-19 21:48:50 +1000

    Bump dotnet-sdk8 cask to 8.0.200

--- Casks/dotnet-sdk8.rb
+++ Casks/dotnet-sdk8.rb
@@ -1,15 +1,15 @@
 cask "dotnet-sdk8" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.101,8.0.1"
+  version "8.0.200,8.0.2"
   sha256 :no_check
 
-  url "https://github.com/fluffynuts/homebrew-dotnet-sdk-versions/raw/master/META.md"
+  url "https://github.com/isen-ng/homebrew-dotnet-sdk-versions/raw/master/META.md"
   name ".NET Core SDK #{version.csv.first}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
-  homepage "https://github.com/fluffynuts/homebrew-dotnet-sdk-versions"
+  homepage "https://github.com/isen-ng/homebrew-dotnet-sdk-versions"
 
-  depends_on cask: "dotnet-sdk8-0-100"
+  depends_on cask: "dotnet-sdk8-0-200"
   depends_on macos: ">= :catalina"
 
   stage_only true
