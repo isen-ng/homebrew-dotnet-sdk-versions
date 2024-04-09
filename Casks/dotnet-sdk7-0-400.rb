e8f8f8d06b9bf64e835775dd5545840bd58d7ccc
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-04-09 22:04:16 +0300

    [Auto] update Casks/dotnet-sdk7-preview.rb from 7.0.407 to 7.0.408

--- Casks/dotnet-sdk7-0-400.rb
+++ Casks/dotnet-sdk7-0-400.rb
@@ -1,12 +1,12 @@
 cask "dotnet-sdk7-0-400" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.0.407,7.0.17"
+  version "7.0.408,7.0.18"
 
-  sha256_x64 = "871495b45d4117f22570c6f31c0469eb64d0b7f47a3ef27c79964cf1f9214836"
-  sha256_arm64 = "3eb3f4775a5027d1ff43a107d103607384711ba80ffaf8eb0864141810a5601e"
-  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/76919cab-fb8f-4726-b4cc-6c711dd99eb2/f64d22a2adf0426490bd22507bcef35d/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
-  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/64222b05-2138-42c3-b101-08c814b2f6e3/49781e5316479099a9441c4506cd1e5f/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
+  sha256_x64 = "ca3e72f9dab83dba6194bff886711483cb3c590cf9c6bbcdf8d0dbb6e1622bfd"
+  sha256_arm64 = "04dd3dab6265f299059e14aa87d6e26dd3207fb78fae6b8e768fdc789c9e6a80"
+  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/a54f7102-d834-48b0-b6f4-32d9d215dd51/57c2418d3d1d7be290e032e535af2d13/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
+  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/5cd047f7-899a-4e00-9c86-d79fe7fe3d6e/82f1cb63ebebe92d084cfc7ab6204b6f/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
 
   on_arm do
     sha256 sha256_arm64
