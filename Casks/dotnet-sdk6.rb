a3217b7f2d2b18289d60b125fdfd0eaf235bf84b
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-04-10 04:19:58 +0300

    [Auto] update Casks/dotnet-sdk6-preview.rb from 6.0.420 to 6.0.421 (#327)

--- Casks/dotnet-sdk6.rb
+++ Casks/dotnet-sdk6.rb
@@ -1,7 +1,7 @@
 cask "dotnet-sdk6" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.0.420,6.0.28"
+  version "6.0.421,6.0.29"
   sha256 :no_check
 
   url "https://github.com/isen-ng/homebrew-dotnet-sdk-versions/raw/master/META.md"
