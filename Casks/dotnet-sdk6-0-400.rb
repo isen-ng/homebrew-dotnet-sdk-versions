2fad30642f2c9c3188776f5060e7eb2539c13563
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2024-05-29 17:18:07 +0000

    [Auto] update Casks/dotnet-sdk6-0-400.rb from 6.0.422 to 6.0.423

--- Casks/dotnet-sdk6-0-400.rb
+++ Casks/dotnet-sdk6-0-400.rb
@@ -1,12 +1,12 @@
 cask "dotnet-sdk6-0-400" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.0.422,6.0.30"
+  version "6.0.423,6.0.31"
 
-  sha256_x64 = "960ece197a4f98d742eff5220c4d581f1cc6d4e2b70bd685015ce47110f57c2b"
-  sha256_arm64 = "2f8db7811bd72a80f8333e6ad9b682a42a4ba862ec860620716528f91339c5ec"
-  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/ce675d3b-f53e-4638-a758-6c2c1fff25dd/ca400cd93ebb189399599589ad6c352c/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
-  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/6427e918-a7a3-4c2c-81f3-15f92e61e661/468d9aaaff48719810e170b0ccf679e8/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
+  sha256_x64 = "19f0298720aa2504af2375f150015d98ed391548666253fdfafd3a90782aaaef"
+  sha256_arm64 = "e231893bdb90b948b35710abd1be71bb5b876a639dd79a9c2159ac7a226b1122"
+  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/7985c7ce-12d1-4180-9e95-3cf81790e958/633824a3c4228754b45106040302a5b2/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
+  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/8a43cb29-3cfd-41e2-8c80-46ec7ae7192d/3e460e7f35b80aefb18b0d1a90849981/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
 
   on_arm do
     sha256 sha256_arm64
