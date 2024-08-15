285e6198e3b7848aeaaec829c1702d9692e3ef3c
Benjamin Evenson <2031163+benjiro@users.noreply.github.com>
2024-08-15 10:41:55 +1000

    Make default dotnet 8 package 8.0.400|8.0.8
    Signed-off-by: Benjamin Evenson <2031163+benjiro@users.noreply.github.com>

--- Casks/dotnet-sdk8.rb
+++ Casks/dotnet-sdk8.rb
@@ -1,7 +1,7 @@
 cask "dotnet-sdk8" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.303,8.0.7"
+  version "8.0.400,8.0.8"
   sha256 :no_check
 
   url "https://github.com/isen-ng/homebrew-dotnet-sdk-versions/raw/master/META.md"
@@ -9,7 +9,7 @@ cask "dotnet-sdk8" do
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://github.com/isen-ng/homebrew-dotnet-sdk-versions"
 
-  depends_on cask: "dotnet-sdk8-0-300"
+  depends_on cask: "dotnet-sdk8-0-400"
   depends_on macos: ">= :catalina"
 
   stage_only true
