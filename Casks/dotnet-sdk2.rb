1e68167f7523f1ecb4fea8888499d0293ba263a6
Isen <3904043+isen-ng@users.noreply.github.com>
2024-09-03 14:12:11 +0800

    [Auto] Update meta casks (#365)

--- Casks/dotnet-sdk2.rb
+++ Casks/dotnet-sdk2.rb
@@ -2,10 +2,10 @@ cask "dotnet-sdk2" do
   version "2.2.402,2.2.7"
   sha256 :no_check
 
-  url "https://github.com/fluffynuts/homebrew-dotnet-sdk-versions/raw/master/META.md"
+  url "https://github.com/isen-ng/homebrew-dotnet-sdk-versions/raw/master/META.md"
   name ".NET Core SDK #{version.csv.first}"
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
-  homepage "https://github.com/fluffynuts/homebrew-dotnet-sdk-versions"
+  homepage "https://github.com/isen-ng/homebrew-dotnet-sdk-versions"
 
   depends_on cask: "dotnet-sdk2-2-400"
   depends_on macos: ">= :sierra"
