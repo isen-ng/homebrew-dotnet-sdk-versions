047d3822a54b068f2e5cbd89b5b0018dcbdf2b20
Benjamin Evenson <2031163+benjiro@users.noreply.github.com>
2025-02-14 09:01:29 +1000

    Add cask for dotnet sdk 9.0.200|9.0.2
    Default dotnet-sdk9 cask to 9.0.2
    
    Signed-off-by: Benjamin Evenson <2031163+benjiro@users.noreply.github.com>

--- Casks/dotnet-sdk9.rb
+++ Casks/dotnet-sdk9.rb
@@ -1,7 +1,7 @@
 cask "dotnet-sdk9" do
   arch arm: "arm64", intel: "x64"
 
-  version "9.0.102,9.0.1"
+  version "9.0.200,9.0.2"
   sha256 :no_check
 
   url "https://github.com/isen-ng/homebrew-dotnet-sdk-versions/raw/master/META.md"
@@ -9,7 +9,7 @@ cask "dotnet-sdk9" do
   desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
   homepage "https://github.com/isen-ng/homebrew-dotnet-sdk-versions"
 
-  depends_on cask: "dotnet-sdk9-0-100"
+  depends_on cask: "dotnet-sdk9-0-200"
   depends_on macos: ">= :ventura"
 
   stage_only true
