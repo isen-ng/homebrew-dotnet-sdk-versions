e229ab48d33a4c8cf21b9cf54291abf7f30871e1
Weihan Li <weihanli@outlook.com>
2025-03-11 13:24:58 +0800

    Update dotnet-sdk10-preview.rb
    update sha512 => sha256

--- Casks/dotnet-sdk10-preview.rb
+++ Casks/dotnet-sdk10-preview.rb
@@ -3,18 +3,18 @@ cask "dotnet-sdk10-preview" do
 
   version "10.0.100-preview.1.25120.13"
 
-  sha512_x64 = "29900a2b93424661ba0300671be017629ca5c5183cdddbeaecae2666f8a5ea6c26e415b7f2286a6644efa27523ebb0f61080b9fad82f85dc9c648344af36268d"
-  sha512_arm64 = "14f83d81cbafcc572a70100037c677911368970e4b20dd471d69595813b688615577b393cb4f8579b284493341b7642ad2f2d00171dd9d1d40d35e886e2ed2dc"
+  sha256_x64 = "29900a2b93424661ba0300671be017629ca5c5183cdddbeaecae2666f8a5ea6c26e415b7f2286a6644efa27523ebb0f61080b9fad82f85dc9c648344af36268d"
+  sha256_arm64 = "14f83d81cbafcc572a70100037c677911368970e4b20dd471d69595813b688615577b393cb4f8579b284493341b7642ad2f2d00171dd9d1d40d35e886e2ed2dc"
   url_x64 = "https://download.visualstudio.microsoft.com/download/pr/80339095-1418-4792-9d81-d7b8dc44a436/d8f1d068666055023d1c98e6d4f8fd60/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
   url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/a826c444-678e-487e-8c4e-ebfc14afca99/d22a35c78682369beb65d20afc995a7e/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
 
   on_arm do
-    sha512 sha512_arm64
+    sha256 sha256_arm64
 
     url url_arm64
   end
   on_intel do
-    sha512 sha512_x64
+    sha256 sha256_x64
 
     url url_x64
   end
