99a218dbd1f2ef62b2c3858e0cdf493072bf4011
Isen <3904043+isen-ng@users.noreply.github.com>
2025-03-14 04:24:00 +0800

    [Auto] Update meta casks (#406)

--- Casks/dotnet-sdk9.rb
+++ Casks/dotnet-sdk9.rb
@@ -1,7 +1,7 @@
 cask "dotnet-sdk9" do
   arch arm: "arm64", intel: "x64"
 
-  version "9.0.200,9.0.2"
+  version "9.0.201,9.0.3"
   sha256 :no_check
 
   url "https://github.com/isen-ng/homebrew-dotnet-sdk-versions/raw/master/META.md"
