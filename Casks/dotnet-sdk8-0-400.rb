2449a35e50871b66c4bac3ecdb074c4706fd1fe2
Isen <3904043+isen-ng@users.noreply.github.com>
2025-03-13 10:40:36 +0800

    [Auto] update Casks/dotnet-sdk8-0-400.rb from 8.0.406 to 8.0.407 (#401)

--- Casks/dotnet-sdk8-0-400.rb
+++ Casks/dotnet-sdk8-0-400.rb
@@ -1,12 +1,12 @@
 cask "dotnet-sdk8-0-400" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.406,8.0.13"
+  version "8.0.407,8.0.14"
 
-  sha256_x64 = "57dd68791875751aabe65d4c8612b29597ab2c218a7d85c4aa4dd4da7f7bf06d"
-  sha256_arm64 = "ba24b1867f8ec375cb0c125acbc1343938ee59efc243c6d96aa726a5cf23d1cf"
-  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/d300f28c-e59b-43ec-bb57-b6e0320a86b2/a1f346317df06c5d8b8bf9785c9090e9/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
-  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/b54de232-c38f-40ba-8ae7-51080435c258/5482d86b044fc3ec602685acde2993f8/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
+  sha256_x64 = "b09e6c16650da962e98e761c6134c31a04ea92591a40a58ec6dc211edb464e49"
+  sha256_arm64 = "6cad4e5449a37df79cbd278e0ce2c6b380c3449a708c77d811ba9b1a4383ea0b"
+  url_x64 = "https://download.visualstudio.microsoft.com/download/pr/36cd1a79-ccbe-4e86-b2ea-d4b45aecae1d/baee9ad982dc7c8058ec825978bf2400/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
+  url_arm64 = "https://download.visualstudio.microsoft.com/download/pr/d798274b-2bfe-48d7-8f0f-70b15a0198f0/b2d3de9536ce33aea258211b8ba55c20/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
 
   on_arm do
     sha256 sha256_arm64
