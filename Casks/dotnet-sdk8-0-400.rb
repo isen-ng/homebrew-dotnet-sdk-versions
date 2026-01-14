d9901332e18d1be82b16afd27baf9492deeddada
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2026-01-14 22:14:21 +0000

    [Auto] update Casks/dotnet-sdk8-0-400.rb from 8.0.416 to 8.0.417

--- Casks/dotnet-sdk8-0-400.rb
+++ Casks/dotnet-sdk8-0-400.rb
@@ -1,10 +1,10 @@
 cask "dotnet-sdk8-0-400" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.416,8.0.22"
+  version "8.0.417,8.0.23"
 
-  sha256_x64 = "515697ec0c1ad9dd65d35b0ceef1139e6c80d1c343be41b62e60204ec35552ee"
-  sha256_arm64 = "c53a29286990d8ad8444dd052cd0fb480983afb138a5fee0a06d427a3b5e4ee2"
+  sha256_x64 = "e3fe62ef220cdeed74dbc2dfcaee47ebb6e48d54aac8d39be4773d8088659051"
+  sha256_arm64 = "849c32bce455875a10d0a18b22ebe0c9708a0584a7bd7e2ce0f8a646220fb140"
   url_x64 = "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version.csv.first}/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
   url_arm64 = "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version.csv.first}/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
 
