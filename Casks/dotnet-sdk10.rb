014938a76bc83f400aa482433040f110cef298f7
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2026-01-15 19:20:49 +0000

    [Auto] Update meta casks

--- Casks/dotnet-sdk10.rb
+++ Casks/dotnet-sdk10.rb
@@ -10,7 +10,7 @@ cask "dotnet-sdk10" do
   homepage "https://github.com/isen-ng/homebrew-dotnet-sdk-versions"
 
   depends_on cask: "dotnet-sdk10-0-100"
-  depends_on macos: ">= :sonoma"
+  depends_on macos: ">= :monterey"
 
   stage_only true
 end
--- Casks/dotnet-sdk8.rb
+++ Casks/dotnet-sdk8.rb
@@ -10,7 +10,7 @@ cask "dotnet-sdk8" do
   homepage "https://github.com/isen-ng/homebrew-dotnet-sdk-versions"
 
   depends_on cask: "dotnet-sdk8-0-400"
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :big_sur"
 
   stage_only true
 end
