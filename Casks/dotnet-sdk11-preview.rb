55d06b84acf7dbc2b48acc710db19afb3f01d3c7
Weihan Li <weihanli@outlook.com>
2026-03-18 21:39:10 +0800

    add dotnet-sdk-11-preview

---
+++ Casks/dotnet-sdk11-preview.rb
@@ -0,0 +1,47 @@
+cask "dotnet-sdk11-preview" do
+  arch arm: "arm64", intel: "x64"
+
+  version "11.0.100-preview.2.26159.112,11.0.100-preview.2.26159.112"
+
+  sha256_x64 = "babdb130049134647ae373d36f9e46e8df10b6b92ec873ea713fb514972bd06229fe100f76c3c76677cbc98f61e652606aba8d558589cb6ce0275bc99b16c422"
+  sha256_arm64 = "83e38acf9b23886305dc5ef5cd4b18c2e8c95f23f2f3fe3959b495037fdbd391ac8c1dbc02713815de3f1276a586eda370dd1e262fc541e68d6427521d028501"
+  url_x64 = "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version.csv.first}/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
+  url_arm64 = "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version.csv.second}/dotnet-sdk-#{version.csv.second}-osx-arm64.pkg"
+
+  on_arm do
+    sha256 sha256_arm64
+
+    url url_arm64
+  end
+  on_intel do
+    sha256 sha256_x64
+
+    url url_x64
+  end
+
+  name ".NET SDK #{version.csv.first}"
+  desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
+  homepage "https://www.microsoft.com/net/core#macos"
+
+  livecheck do
+    skip "See https://github.com/isen-ng/homebrew-dotnet-sdk-versions/blob/master/CONTRIBUTING.md#automatic-updates"
+  end
+
+  depends_on macos: ">= :monterey"
+
+  pkg "dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
+
+  uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.csv.first}.component.osx.#{arch}"
+
+  zap pkgutil: [
+        "com.microsoft.dotnet.hostfxr.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.pack.apphost.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.sharedhost.component.osx.#{arch}",
+      ],
+      trash:   ["~/.dotnet", "~/.nuget", "/etc/paths.d/dotnet", "/etc/paths.d/dotnet-cli-tools"]
+i
+  caveats "Uninstalling the official dotnet-sdk casks will remove the shared runtime dependencies, " \
+          "so you'll need to reinstall the particular version cask you want from this tap again " \
+          "for the `dotnet` command to work again."
+end
