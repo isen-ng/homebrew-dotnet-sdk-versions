12cce305ab5b294ad1649a7fdfa603942f945382
Weihan Li <weihanli@outlook.com>
2026-03-19 15:10:42 +0800

    add dotnet-sdk-11-preview (#494)
    * add dotnet-sdk-11-preview
    
    * update version usage
    
    * update x64 sha256 hash
    
    * ci: upgrade checkout version to fix warning
    
    * ci: update all checkout action
    
    * update sha256_arm64 for testing
    
    * update runtime version
    
    * update sha256_arm64 hash

---
+++ Casks/dotnet-sdk11-preview.rb
@@ -0,0 +1,47 @@
+cask "dotnet-sdk11-preview" do
+  arch arm: "arm64", intel: "x64"
+
+  version "11.0.100-preview.2.26159.112,11.0.0-preview.2.26159.112"
+
+  sha256_x64 = "7a03a5d3c210b9f567433b350834965ac8e81365f3069e7a8d888fe189e77e3c"
+  sha256_arm64 = "14843284bbee7a5a2b1fa814e2d6809404e5746789fac6151a2a8fd07bed786d"
+  url_x64 = "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version.csv.first}/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
+  url_arm64 = "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version.csv.first}/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
+
+  on_arm do
+    sha256 sha256_arm64
+
+    url url_arm64
+  end
+  on_intel do
+    sha256 sha256_x64
+
+    url url_x64
+  end
+
+  name ".NET SDK #{version.csv.first}"
+  desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
+  homepage "https://www.microsoft.com/net/core#macos"
+
+  livecheck do
+    skip "See https://github.com/isen-ng/homebrew-dotnet-sdk-versions/blob/master/CONTRIBUTING.md#automatic-updates"
+  end
+
+  depends_on macos: ">= :monterey"
+
+  pkg "dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
+
+  uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.csv.first}.component.osx.#{arch}"
+
+  zap pkgutil: [
+        "com.microsoft.dotnet.hostfxr.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.pack.apphost.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.sharedhost.component.osx.#{arch}",
+      ],
+      trash:   ["~/.dotnet", "~/.nuget", "/etc/paths.d/dotnet", "/etc/paths.d/dotnet-cli-tools"]
+
+  caveats "Uninstalling the official dotnet-sdk casks will remove the shared runtime dependencies, " \
+          "so you'll need to reinstall the particular version cask you want from this tap again " \
+          "for the `dotnet` command to work again."
+end
