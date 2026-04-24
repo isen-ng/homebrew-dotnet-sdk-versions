ba3734beea653156d652f69f5c35c4a426770967
isen <3904043+isen-ng@users.noreply.github.com>
2026-04-24 11:47:16 +0800

    fix macos for intel only casks

--- Casks/dotnet-sdk2.rb
+++ Casks/dotnet-sdk2.rb
@@ -8,7 +8,7 @@ cask "dotnet-sdk2" do
   homepage "https://github.com/isen-ng/homebrew-dotnet-sdk-versions"
 
   depends_on cask: "dotnet-sdk2-2-400"
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :any
 
   stage_only true
 end
