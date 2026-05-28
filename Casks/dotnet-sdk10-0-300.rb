f0844cccaa1755ca817409003ef309baa3694023
Alexander Groß <agross@therightstuff.de>
2026-05-28 10:19:59 +0200

    Add support for dotnet-sdk10-0-300 (#538)

---
+++ Casks/dotnet-sdk10-0-300.rb
@@ -0,0 +1,47 @@
+cask "dotnet-sdk10-0-300" do
+  arch arm: "arm64", intel: "x64"
+
+  version "10.0.300,10.0.8"
+
+  sha256_x64 = "23bc0e5337525c2491328f66922b27f4aea46c8584edf727d8d9ddf7b6ac5a84"
+  sha256_arm64 = "508bf513a5753cc269a382354b004baccdceb71c17fa7de5d9382b2829e69b50"
+  url_x64 = "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version.csv.first}/dotnet-sdk-#{version.csv.first}-osx-x64.pkg"
+  url_arm64 = "https://builds.dotnet.microsoft.com/dotnet/Sdk/#{version.csv.first}/dotnet-sdk-#{version.csv.first}-osx-arm64.pkg"
+
+  on_arm do
+    sha256 sha256_arm64
+
+    url url_arm64
+  end
+  on_intel do
+    sha256 sha256_x64
+
+    url url_x64
+  end
+
+  name ".NET Core SDK #{version.csv.first}"
+  desc "This cask follows releases from https://github.com/dotnet/core/tree/master"
+  homepage "https://www.microsoft.com/net/core#macos"
+
+  livecheck do
+    skip "See https://github.com/isen-ng/homebrew-dotnet-sdk-versions/blob/master/CONTRIBUTING.md#automatic-updates"
+  end
+
+  depends_on macos: :monterey
+
+  pkg "dotnet-sdk-#{version.csv.first}-osx-#{arch}.pkg"
+
+  uninstall pkgutil: "com.microsoft.dotnet.dev.#{version.csv.first}.component.osx.#{arch}"
+
+  zap pkgutil: [
+        "com.microsoft.dotnet.hostfxr.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.pack.apphost.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.#{version.csv.second}.component.osx.#{arch}",
+        "com.microsoft.dotnet.sharedhost.component.osx.#{arch}",
+      ],
+      trash:   ["~/.dotnet", "~/.nuget", "/etc/paths.d/dotnet", "/etc/paths.d/dotnet-cli-tools"]
+
+  caveats "Uninstalling the offical dotnet-sdk casks will remove the shared runtime dependencies, " \
+          "so you'll need to reinstall the particular version cask you want from this tap again " \
+          "for the `dotnet` command to work again."
+end
