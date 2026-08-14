ee7548e1dea55fba103d0762a812a91015e6d54c
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2026-08-14 02:48:31 +0000

    [Auto] Update meta casks

--- Casks/dotnet-sdk8.rb
+++ Casks/dotnet-sdk8.rb
@@ -1,7 +1,7 @@
 cask "dotnet-sdk8" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.423,8.0.29"
+  version "8.0.424,8.0.30"
   sha256 :no_check
 
   url "https://github.com/isen-ng/homebrew-dotnet-sdk-versions/raw/master/META.md"
